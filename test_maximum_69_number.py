e43d8438355f0518c4a8a539696c910ec941138d
Yuliia Antonova <yuliia.antonova1@gmail.com>
2021-03-22 10:48:31 +0200

    # 1323. Maximum 69 Number Testing

---
+++ test_maximum_69_number.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from maximum_69_number import Solution
+
+class TestSolution(TestCase):
+    def test_maximum69number(self):
+        self.assertTrue(Solution.maximum69Number(self,9669))
