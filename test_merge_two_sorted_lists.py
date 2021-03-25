a4366f0b360627d9ac9d99a63d394234d038606a
Yuliia Antonova <yuliia.antonova1@gmail.com>
2021-03-25 22:54:07 +0200

    # 1389. Create Target Array Testing

---
+++ test_merge_two_sorted_lists.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from merge_two_sorted_lists import Solution
+
+class TestSolution(TestCase):
+    def test_merge_two_lists(self):
+        # self.assertTrue(Solution.mergeTwoLists(self,[1,2,4],[1,3,4]))
+         self.assertTrue(Solution.mergeTwoLists(self,[],[]))
+        # self.assertTrue(Solution.mergeTwoLists(self,[],[0]))
+
+
+
